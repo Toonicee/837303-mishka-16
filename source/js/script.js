00fb305db4d4a403e31729430c1691ca11a27c1c
Сергей_Я <tooniccee@gmail.com>
2019-07-01 20:40:41 +0800

    добавление слайдера в отзывы

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,12 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
-
-
+var bestButton = document.querySelector('.best-product__button');
+var mPopup = document.querySelector('.modal');
+var modalBtn = document.querySelector('.modal-button');
+var btn_prev = document.querySelector('.slider .reviews__prev');
+var btn_next = document.querySelector('.slider .reviews__next');
+var reviews = document.querySelectorAll('.slider .slider__list');
+var i = 0;
 
   navMain.classList.remove('main-nav--nojs');
 
@@ -15,10 +20,6 @@ var navToggle = document.querySelector('.main-nav__toggle');
     }
   });
 
-var bestButton = document.querySelector('.best-product__button');
-var mPopup = document.querySelector('.modal');
-var modalBtn = document.querySelector('.modal-button');
-
   bestButton.addEventListener("click", function (evt) {
     evt.preventDefault();
     mPopup.classList.remove("modal");
@@ -28,3 +29,26 @@ var modalBtn = document.querySelector('.modal-button');
     evt.preventDefault();
     mPopup.classList.add("modal")
   });
+
+//Слайдер
+    btn_prev.onclick = function(){
+         reviews[i].classList.remove('showed');
+         i--;
+
+         if(i < 0){
+             i = reviews.length - 1;
+         }
+
+         reviews[i].classList.add('showed');
+    }
+
+    btn_next.onclick = function(){
+         reviews[i].classList.remove('showed');
+         i++;
+
+         if(i >= reviews.length){
+             i = 0;
+         }
+
+         reviews[i].classList.add('showed');
+    }
